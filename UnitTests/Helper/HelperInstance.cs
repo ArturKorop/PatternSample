1dd2e75ef285988a88acb4845b297ddad24d8d1e
Artur Korop <artyr.korop@gmail.com>
2013-09-27 11:40:24 +0300

    minor (UnitTests) : added methods to HelperInstance

--- UnitTests/Helper/HelperInstance.cs
+++ UnitTests/Helper/HelperInstance.cs
@@ -1,10 +1,16 @@
-﻿using Common.Interfaces;
+﻿using System;
+using Common.Code;
+using Common.Code.Configuration;
+using Common.Interfaces;
+using Microsoft.Practices.Unity;
 using Moq;
 
 namespace UnitTests.Helper
 {
     public static class HelperInstance
     {
+        public static string Result;
+
         public static ITextProvider EmptyTextProvider
         {
             get
@@ -15,6 +21,25 @@ namespace UnitTests.Helper
 
                 return mock.Object;
             }
+        }
+
+        public static ITextProvider GetTestTextProvider()
+        {
+            var mock = new Mock<ITextProvider>();
+            mock.Setup(x => x.WriteLine(It.IsAny<string>(), It.IsAny<object[]>())).Callback<string, object[]>(
+                (format, args) =>
+                    {
+                        Result += String.Format(format, args);
+                    });
+            mock.Setup(x => x.Wait());
+
+            return mock.Object;
+        }
+
+        public static void InitClassWithITextProvider(ITextProvider provider)
+        {
+            DIServiceLocator.Current.RegisterInstance(typeof (ITextProvider), provider);
+            Support.Configure();
         }
     }
 }
