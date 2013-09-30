6cbc1c6c6137d4cd5941214384541be3218c99d4
Artur Korop <artyr.korop@gmail.com>
2013-09-30 15:24:04 +0300

    minor (UnitTests) : added unit tests for pattern TemplateMethod

---
+++ UnitTests/PatternLibraryTests/TemplateMethodTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.Protected;
+using PatternLibrary.Patterns.TemplateMethod.Abstract;
+
+namespace UnitTests.PatternLibraryTests
+{
+    [TestClass]
+    public class TemplateMethodTests
+    {
+        [ClassInitialize]
+        public static void InitClass(TestContext context)
+        {
+            Helper.HelperInstance.InitClassWithITextProvider(Helper.HelperInstance.EmptyTextProvider);
+        }
+
+        [TestMethod]
+        public void TemplateMethodTest()
+        {
+            var mockCaffeineBeverage = new Mock<CaffeineBeverage>();
+            mockCaffeineBeverage.Protected().Setup<bool>("CustomerWantsCondiments").Returns(true);
+            var target = mockCaffeineBeverage.Object;
+            target.PrepareReceipt();
+
+            mockCaffeineBeverage.Protected().Verify("Brew", Times.Once());
+            mockCaffeineBeverage.Protected().Verify("AddCondiments", Times.Once());
+            mockCaffeineBeverage.Protected().Verify("CustomerWantsCondiments", Times.Once());
+        }
+    }
+}
