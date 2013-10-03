57e990f2a7f8f40a20884de5af7bc0fdd37c9c0f
Artur Korop <artyr.korop@gmail.com>
2013-10-03 12:47:03 +0300

    Major (Solution) : added project which run wcf service from PatternProxy

---
+++ PatternProxyStarter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using PatternProxy;
+
+
+namespace PatternProxyStarter
+{
+    class Program
+    {
+        readonly static Uri BaseAddress = new Uri("http://localhost:8080/GumballMachineService");
+
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(MainService), BaseAddress))
+            {
+                var smb = new ServiceMetadataBehavior
+                    {
+                        HttpGetEnabled = true,
+                        MetadataExporter = {PolicyVersion = PolicyVersion.Policy15}
+                    };
+
+                host.Description.Behaviors.Add(smb);
+                host.Open();
+
+                Console.WriteLine("The service is ready at {0}", BaseAddress);
+                Console.WriteLine("Press <q> to stop the service.");
+                Console.ReadKey();
+               
+                host.Close();
+            }
+        }
+    }
+}
