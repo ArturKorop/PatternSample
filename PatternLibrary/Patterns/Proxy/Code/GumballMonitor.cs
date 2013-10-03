079695ab42418b9f35c5613de19051ab4670c299
Artur Korop <artyr.korop@gmail.com>
2013-10-03 12:51:16 +0300

    Major (PatternLibrary) : added pattern Proxy

---
+++ PatternLibrary/Patterns/Proxy/Code/GumballMonitor.cs
@@ -0,0 +1,29 @@
+﻿using System.ServiceModel;
+using Common.Code;
+using PatternLibrary.GumballMachineService;
+
+namespace PatternLibrary.Patterns.Proxy.Code
+{
+    public class GumballMonitor
+    {
+        private readonly GumballMachineServiceClient _client;
+
+        public GumballMonitor()
+        {
+            _client = new GumballMachineServiceClient(new BasicHttpBinding(),
+                                                      new EndpointAddress(
+                                                          "http://localhost:8080/GumballMachineService"));
+            _client.Open();
+        }
+
+        public void PrintStatus()
+        {
+            _client.GetStatus().P();
+        }
+
+        public void GetGumball()
+        {
+            _client.GetGumball();
+        }
+    }
+}
---
+++ PatternLibrary/Patterns/Proxy/ProxyStarter.cs
@@ -0,0 +1,27 @@
+﻿using Common.Code;
+using PatternLibrary.Patterns.Proxy.Code;
+
+namespace PatternLibrary.Patterns.Proxy
+{
+    /// <summary>
+    /// For running this pattern need doing some steps:
+    /// 1) Build PatternProxyStarter
+    /// 2) Copy files from build folder of PatternProxyStarter to other place
+    /// 3) Run PatternProxyStarter.exe
+    /// 4) Check, that adderess in PatternProxyStarter equals address in GumballMonitor
+    /// 5) Run ProxyStarter and see results of its work
+    /// </summary>
+    [Runnable]
+    public class ProxyStarter
+    {
+        public static void Start()
+        {
+            var monitor = new GumballMonitor();
+            monitor.PrintStatus();
+            monitor.GetGumball();
+            monitor.GetGumball();
+            monitor.GetGumball();
+            monitor.PrintStatus();
+        } 
+    }
+}
